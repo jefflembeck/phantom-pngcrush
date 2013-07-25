21a3a91d74e753728e56719b7bc8aff8eee248e8
Jeffrey Lembeck <jlembeck@gmail.com>
2013-07-25 11:49:47 -0700

    break out into lib for easier exporting

--- phantom-pngcrush.js
+++ phantom-pngcrush.js
@@ -16,54 +16,18 @@ phantom args sent in:
 (function(){
   "use strict";
 
-  var execFile = require( "child_process" ).execFile;
-  var fs = require( "fs" );
+  var crusher = require( "./lib/pngcrusher" );
 
   var options = {
     input: phantom.args[0],
     output: phantom.args[1]
   };
 
-  var exit = function(){
-    phantom.exit(0);
-  };
-
-  //if input is a directory, output should be a directory as well, cycle
-  //through entire folder, slam all of the stuff out
-
   if( !options.input ){
     console.log( "REQUIRES AN INPUT" );
-    exit();
+    phantom.exit();
   }
 
-  var input = options.input,
-    absInput = fs.absolute( input ),
-    output = options.output,
-    filelist, relFiles, filequery;
-
-  if( fs.isDirectory( absInput ) ){
-    if( !fs.exists( output ) ){
-      fs.makeDirectory( output );
-    }
-    filelist = fs.list( absInput );
-    relFiles = filelist.slice( 2 );
-    filequery = relFiles.map( function( file ){
-      return absInput + "/" + file;
-    });
-    var arr = [ "-d", output ];
-    arr.push.apply( arr , filequery );
-
-    execFile( "pngcrush", arr , null, function( err, stdout, stderr ){
-      if( err ){
-        console.log( err );
-        exit( 1 );
-      }
-      exit();
-    });
-  } else {
-    execFile( "pngcrush", [ input , options.output ] , null , function( err, stdout, stderr ){
-      exit();
-    });
-  }
+  crusher.crush( options );
 
 }());
